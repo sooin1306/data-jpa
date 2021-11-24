5d356fb042d1811c65efc825a81b5a6fc3768201
sion <sooin92402@gmail.com>
2021-11-24 14:44:05 +0900

    MemberJpaRepository 수정

--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -5,6 +5,9 @@ import study.datajpa.entity.Member;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class MemberJpaRepository {
@@ -14,6 +17,24 @@ public class MemberJpaRepository {
     public Member save (Member member){
         em.persist(member);
         return member;
+    }
+
+    public void delete(Member member){
+        em.remove(member);
+    }
+
+    public List<Member> findAll(){
+        return em.createQuery("select m from Member m", Member.class).getResultList();
+
+    }
+
+    public Optional <Member> findById(Long id){
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);//member가 null일 수도 있음
+    }
+
+    public long count(){
+        return em.createQuery("select count (m) from Member m", Long.class).getSingleResult();
     }
 
     public Member find(Long id){
