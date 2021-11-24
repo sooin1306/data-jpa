98d4449a06f620cd389e3ac6cbc7652738daf6b3
sion <sooin92402@gmail.com>
2021-11-24 17:58:02 +0900

    DataJpaApplication 실행관련 설명 추가

--- src/main/java/study/datajpa/DataJpaApplication.java
+++ src/main/java/study/datajpa/DataJpaApplication.java
@@ -2,8 +2,10 @@ package study.datajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+//@EnableJpaRepositories(basePackages = "study.datajpa.repository")//springboot는 자동으로 설정됨
 public class DataJpaApplication {
 
 	public static void main(String[] args) {
