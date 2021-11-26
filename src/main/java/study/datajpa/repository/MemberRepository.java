2b378d061d80bda86c0f9556c863e306cdf0e7c1
sion <sooin92402@gmail.com>
2021-11-26 13:29:07 +0900

    @EntityGraph 실습

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -4,6 +4,7 @@ package study.datajpa.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -51,4 +52,19 @@ public interface MemberRepository extends JpaRepository<Member,Long> {
     @Modifying//(clearAutomatically = true)설정 시 em.clear(영속성 컨텍스트 초기화) 자동실행//excuteUpdate를 실행하는 어노테이션//
     @Query("update Member m set m.age = m.age+1 where m.age >= :age")
     int bulkAgePlus(@Param("age") int age);
+
+    @Query("select m from Member m left join fetch m.team")
+    List<Member> findMemberFetchJoin();
+
+    @Override
+    @EntityGraph(attributePaths= {"team"})
+    List<Member> findAll();
+
+    @EntityGraph(attributePaths = {"team"})
+    @Query("select m from Member m")
+    List<Member> findMemberEntityGraph();
+
+ //   @EntityGraph(attributePaths = {"team"})
+    @EntityGraph("Member.all")
+    List<Member> findEntityGraphByUsername(@Param("username") String username);
 }
--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -273,8 +273,38 @@ class MemberRepositoryTest {
 
         //then
         assertThat(resultCount).isEqualTo(3);
-
-
     }
 
+    @Test
+    public void findMemberLazy(){
+        //given
+
+            //member1 -> TeamA
+            //member2 -> TeamB
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        teamRepository.save(teamA);
+        teamRepository.save(teamB);
+        Member member1 = new Member("member1",10, teamA);
+        Member member2 = new Member("member2",10, teamB);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        em.flush();
+        em.clear();
+
+        //when N + 1
+        //select Member 1
+//        List<Member> members = memberRepository.findAll();//(EntityGraph 사용= fetchJoin)
+//        List<Member> members = memberRepository.findMemberFetchJoin();
+        List<Member> members = memberRepository.findEntityGraphByUsername("member1");
+        for (Member member : members) {
+            System.out.println("member = " + member.getUsername());
+            System.out.println("member.TeamClass = " + member.getTeam().getClass());
+            System.out.println("member = " + member.getTeam().getName());
+
+        }
+        //then
+    }
 }
