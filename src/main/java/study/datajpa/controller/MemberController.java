74e409d4d0ab32ada52bae9c3e956099565f59fc
sion <sooin92402@gmail.com>
2021-11-29 17:38:08 +0900

    Web 확장 - 페이징 (DTO 반환) 실습

--- src/main/java/study/datajpa/controller/MemberController.java
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -2,10 +2,13 @@ package study.datajpa.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.repository.MemberRepository;
 
@@ -30,9 +33,9 @@ public class MemberController {
     }
 
     @GetMapping("/members")
-    public Page<Member> list (Pageable pageable) {
-        Page<Member> page = memberRepository.findAll(pageable);
-        return page;
+    public Page<MemberDto> list (@PageableDefault(size = 5) Pageable pageable) {
+        return memberRepository.findAll(pageable)
+                .<MemberDto>map(MemberDto::new);
         //return memberRepository.findAll(pageable); //인라인 방법
     }
 
